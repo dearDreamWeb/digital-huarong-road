d0b1c4629afcd421f5284f374108e8f82725d74c
wangxiaobao <superwang9527@163.com>
2024-01-19 18:06:01 +0800

    feat: delete log

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -59,14 +59,6 @@ const Index = () => {
   const stepRef = useRef(0);
 
   useEffect(() => {
-    console.log(1111, import.meta.env.VITE_PUBLIC_KEY);
-    digital({
-      gameName: 'digitalHuarongRoad',
-      subName: selectOption,
-      score: encrypt('2222'),
-      userId: '123123',
-      nickName: 'test',
-    });
     getNickname();
     randomLayout();
     let _app = new PIXI.Application({
